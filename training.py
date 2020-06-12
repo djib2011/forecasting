693aa35c80c3ef22c0448c3add8c987c0ca9e76a
djib2011 <thanostagaris@gmail.com>
2020-06-12 10:15:43 +0000

    Added requirements.txt

--- training.py
+++ training.py
@@ -34,10 +34,10 @@ else:
 # define grid search
 input_seq_length = hp.HParam('input_seq_length', hp.Discrete([inp_length]))
 output_seq_length = hp.HParam('output_seq_length', hp.Discrete([out_length]))
-bottleneck_size = hp.HParam('bottleneck_size', hp.Discrete([300, 400, 500, 600]))
+bottleneck_size = hp.HParam('bottleneck_size', hp.Discrete([300, 600]))
 bottleneck_activation = hp.HParam('bottleneck_activation', hp.Discrete(['relu']))
 loss_function = hp.HParam('loss_function', hp.Discrete(['mae']))
-direction = hp.HParam('direction', hp.Discrete(['bi', 'bi2', 'conv2', 'comb']))
+direction = hp.HParam('direction', hp.Discrete(['bi', 'bi2', 'conv', 'conv2']))
 
 # define metrics
 reconstruction_loss = metrics.build_reconstruction_loss(overlap=overlap)
@@ -94,7 +94,7 @@ with tf.summary.create_file_writer('logs/tuning').as_default():
 
 
 # Start training loop
-def hyperparam_loop(logs=True, line=False, aug=False):
+def hyperparam_loop(logs=True, line=False, aug=False, epochs=5):
     for inp_seq in input_seq_length.domain.values:
         for out_seq in output_seq_length.domain.values:
             for loss in loss_function.domain.values:
@@ -127,8 +127,9 @@ def hyperparam_loop(logs=True, line=False, aug=False):
                                     model.train_on_batch(x, y)
                                     continue
                                 run(run_name, model_generator=model_mapping[direct],
-                                    hparams=hparams, epochs=5, logs=logs)
+                                    hparams=hparams, epochs=epochs, logs=logs)
 
 
 if __name__ == '__main__':
-    hyperparam_loop(logs=(not args.no_logs), line=args.line, aug=args.aug)
+    epochs = 10 if args.aug else 5
+    hyperparam_loop(logs=(not args.no_logs), line=args.line, aug=args.aug, epochs=epochs)

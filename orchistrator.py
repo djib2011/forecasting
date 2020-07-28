71476bac6c8285c180d6d73c90ec8cf2337d5d6b
djib2011 <thanostagaris@gmail.com>
2020-07-28 09:58:20 +0000

    Restructured data dir.

--- orchistrator.py
+++ orchistrator.py
@@ -1,7 +1,9 @@
 import os
 
-base = 'python training.py -i 18 -o 8 -a '
+os.system('python training.py -i 18 -o 8 --decomposed')
+
+base = 'python training.py -i 18 -o 8 -a {} --decomposed'
 augs = [0.5, 0.67, 0.75, 0.8, 0.9, 0.95]
 
 for a in augs:
-    os.system(base + str(a))
+    os.system(base.format(str(a)))

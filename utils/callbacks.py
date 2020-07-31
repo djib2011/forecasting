db5cdfe06bf7355de9b957431fc31f6f990da5c9
djib2011 <thanostagaris@gmail.com>
2020-07-31 09:59:32 +0300

    Updated SnapshotEnsemble to work on iterations.

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -23,7 +23,7 @@ class CosineAnnealingLearningRateSchedule(tf.keras.callbacks.Callback):
         self.lrates.append(lr)
 
 
-class SnapshotEnsemble(tf.keras.callbacks.Callback):
+class SnapshotEnsembleOld(tf.keras.callbacks.Callback):
     """
     Implementation of Snapshot Ensembles
 
@@ -64,3 +64,100 @@ class SnapshotEnsemble(tf.keras.callbacks.Callback):
             if not os.path.isdir(run_name):
                 os.makedirs(run_name)
             self.model.save(run_name)
+
+
+class SnapshotEnsemble(tf.keras.callbacks.Callback):
+    """
+    Implementation of Snapshot Ensembles
+
+    As described in: https://arxiv.org/pdf/1704.00109.pdf
+    Cosine annealing: https://arxiv.org/pdf/1608.03983.pdf
+    """
+
+    def __init__(self, family_name, steps_per_epoch, n_cycles=10, max_epochs=None, cold_start_id=0):
+
+        super().__init__()
+        self.cycles = n_cycles
+        self.offset = n_cycles * cold_start_id
+        self.epochs = max_epochs
+        self.max_warmup = max_epochs - n_cycles
+        self.max_lr = self.model.optimizer.learning_rate
+        self.lrs = []
+        self.family_name = family_name
+        self.n_iterations = steps_per_epoch
+        self.curr_cycle = 0
+
+        self.stagnant = MetricMonitor(steps=100)
+        self.in_warmup = True
+
+    def cosine_annealing(self, iteration):
+        cos_inner = (np.pi * (iteration % self.n_iterations)) / self.n_iterations
+        return self.max_lr / 2 * (np.cos(cos_inner) + 1)
+
+    def warmup_phase(self, logs):
+        loss = logs.get('loss')
+        self.stagnant(loss)
+        if self.stagnant:
+            self.in_warmup = False
+
+    def on_train_batch_end(self, batch, logs=None):
+        if self.in_warmup:
+            self.warmup_phase(logs)
+            self.lrs.append(self.max_lr)
+        else:
+            lr = self.cosine_annealing(iteration=batch)
+            tf.keras.backend.set_value(self.model.optimizer.learning_rate, lr)
+            self.lrs.append(lr)
+
+    def on_epoch_end(self, epoch, logs=None):
+        if self.in_warmup:
+            if epoch > self.max_warmup:
+                self.in_warmup = False
+        else:
+            run_name = self.family_name + '__{}/best_weights.h5'.format(self.curr_cycle + self.offset)
+            if not os.path.isdir(run_name):
+                os.makedirs(run_name)
+            self.model.save(run_name)
+
+
+class MetricMonitor:
+
+    def __init__(self, steps=10, threshold=0.1):
+        self.n = steps
+        self.metric = []
+        self.full = False
+        self.threshold = threshold
+
+    def __call__(self, val):
+
+        self.metric.append(val)
+
+        if self.full:
+            self.metric.pop(0)
+        else:
+            self.full = self.check_full()
+
+    def check_full(self):
+        return len(self.metric) >= self.n
+
+    def average(self):
+        return np.mean(self.metric)
+
+    def flush(self):
+        self.metric = []
+        self.full = False
+
+    def no_significant_change(self, threshold=None):
+
+        if not self.full:
+            return False
+
+        if not threshold:
+            threshold = self.threshold
+
+        threshold = self.average() * (1 + threshold)
+
+        return (np.max(np.abs(self.metric)) - np.min(np.abs(self.metric))) >= threshold
+
+    def __bool__(self):
+        return self.no_significant_change()

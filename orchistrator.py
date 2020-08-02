5daca00b14eae536ac9b2e5b54935e1df129df27
djib2011 <thanostagaris@gmail.com>
2020-08-02 06:32:18 +0000

    Temporarily duplicated training script to orchistrate training under multiple optimizers.

--- orchistrator.py
+++ orchistrator.py
@@ -1,10 +1,7 @@
 import os
 
-os.system('python training.py -i 18 -o 8 --decomposed')
-
-base = 'python training.py -i 18 -o 8 -a {} --decomposed'
-augs = [0.5, 0.67, 0.75, 0.8, 0.9, 0.95]
-
-for a in augs:
-    os.system(base.format(str(a)))
+os.system('python training.py -i 18 -o 8 -a 0.95')
+os.system('python training2.py -i 18 -o 8 -a 0.95')
+os.system('python training3.py -i 18 -o 8 -a 0.95')
+os.system('python training4.py -i 18 -o 8 -a 0.95')
 
